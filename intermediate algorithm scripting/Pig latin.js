6e26fae1e34434e5224de92ac1d40b70498352d1
Brienna Herold <brienna.herold@gmail.com>
2016-09-15 17:53:56 -0400

    Solve challenge

---
+++ intermediate algorithm scripting/Pig latin.js
@@ -0,0 +1,31 @@
+/*  Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word, 
+moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.  
+
+Input strings are guaranteed to be English words in all lowercase.  */
+
+function translatePigLatin(str) {
+	var vowels = ['a', 'e', 'i', 'o', 'u'];
+	
+	if (vowels.includes(str.charAt(0))) {
+		str += 'way';
+	} else {
+		for (var i = 0; i < str.length; i++) {
+			if (vowels.includes(str.charAt(i))) {
+				str = str.slice(i, str.length) + str.slice(0, i) + 'ay';
+				break;
+			}
+		}
+	}
+	
+  return str;
+  
+}
+
+translatePigLatin("consonant");  // returns 
+translatePigLatin("california");  // returns aliforniacay
+translatePigLatin("algorithm");  // returns algorithmway
+translatePigLatin("glove");  // returns oveglay

dab1b2f5da6c0dd306dd3c3b702bcb343d13a274
Brienna Herold <brienna.herold@gmail.com>
2016-09-15 22:23:12 -0400

    Solve challenge

---
+++ intermediate algorithm scripting/DNA Pairing.js
@@ -0,0 +1,32 @@
+/*  The DNA strand is missing the pairing element. Take each character, 
+get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the 
+provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays 
+are grouped into one encapsulating array.  */
+
+function pairElement(str) {
+  var pairs = {
+    A: "T",
+    C: "G",
+    T: "A",
+    G: "C"
+  };
+  
+  var result = str.split("");
+  for (var i = 0; i < result.length; i++) {
+    var current = result[i];
+    result[i] = [current, pairs[current]];
+  }
+  
+  return result;
+}
+
+pairElement("ATCGA");  // returns [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+pairElement("CTCTA");  // returns [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]

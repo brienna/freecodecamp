fa7fc8e23e57a792c9a6246bf3d68634ccc79487
Brienna Herold <brienna.herold@gmail.com>
2016-11-30 23:10:03 -0500

    Edit style

--- intermediate front end development projects/build a random quote machine/script.js
+++ intermediate front end development projects/build a random quote machine/script.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
+    getQuote();
     $("button").on("click", getQuote);
 });
 
 function getQuote() {
     $.ajax({
         url: "https://api.whatdoestrumpthink.com/api/v1/quotes/random", 
-        cache: false
+        cache: false // for IE so can get new quotes?
     }).done(function(data) {
         var quote = data.message;
-        $("#message").text(quote);
+        $("p").text(quote);
     }).fail(function(err){
         // handle error 
     });
 
-    changeBackgroundColor("body"); 
+    changeBackgroundColor("body");
 }
 
 // Changes an element's background color randomly (not possible in CSS AFAIK)
@@ -21,5 +22,8 @@ function changeBackgroundColor(e) {
     var x = Math.floor(Math.random() * 255);
     var y = Math.floor(Math.random() * 255);
     var z = Math.floor(Math.random() * 255);
-    $(e).css("background-color", "rgb(" + x + ", " + y + ", " + z + ")");
+    var color = "rgb(" + x + ", " + y + ", " + z + ")";
+    $(e).css("background-color", color);
+    $(".btn").css("background-color", color);
+    $("p").css("color", color)
 }
